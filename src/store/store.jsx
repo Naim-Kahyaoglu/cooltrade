0eaa018e465eb67128cb25a8f6a60d3f70f188a4
Naim-Kahyaoglu <naimtrainingweb753@gmail.com>
2024-12-17 17:37:11 +0300

    redux toolkit kaldırıldı redux vanilla ile güncellendi

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,20 +1,21 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { thunk } from 'redux-thunk'; // Changed this line
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { thunk } from 'redux-thunk';
 import logger from 'redux-logger';
 import userReducer from './userSlice';
 import clientReducer from './reducers/clientReducer';
 import productReducer from './reducers/productReducer';
 import shoppingCartReducer from './reducers/shoppingCartReducer';
 
-const store = configureStore({
-  reducer: {
-    user: userReducer,
-    client: clientReducer,
-    product: productReducer,
-    shoppingCart: shoppingCartReducer,
-  },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk, logger),
+const rootReducer = combineReducers({
+  user: userReducer,
+  client: clientReducer,
+  product: productReducer,
+  shoppingCart: shoppingCartReducer,
 });
 
-export default store;
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk, logger)
+);
 
+export default store;
